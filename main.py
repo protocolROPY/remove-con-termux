b2307c00b34baf3712fe13de59cc9543872dc013
Mohammed Al Mahruqi <makmoi197@gmail.com>
2023-06-03 18:09:39 +0400

    5

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ import json
 def removeStartWith(start):
     contacts_output = subprocess.check_output(["termux-contact-list"])
     contacts = json.loads(contacts_output)
-
     for contact in contacts:
         name = contact["name"]
         if name.startswith(start):
             print(str(contact["name"]))
-            # subprocess.call(["termux-contact-remove", str(contact["id"])])
+    print("\n")
+    makeSure = input("Are you sure😨[Y/N]? ")
+    if makeSure.lower() == "y":
+        for contact in contacts:
+            name = contact["name"]
+            if name.startswith(start):
+                    subprocess.call(["termux-contact-remove", str(contact["name"])])
+    else:
+        print("😅bye HHHOOO !")
 
 if __name__ == "__main__":
     subprocess.call(["clear"])
     sw = input("Enter name or start-With you want to remove: ")
-    makeSure = input("Are you sure😨[Y/N]? ")
-    if makeSure.lower() == "y":
-        removeStartWith(sw)
+    removeStartWith(sw)
